09a2a624f081d732632771a67300058b3a69f714
Cheolhyun Mun <zooxop@gmail.com>
2024-10-07 23:56:38 +0900

    fix: 승리 조건 버그 수정

--- src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/EmptyCell.java
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/EmptyCell.java
@@ -26,7 +26,7 @@ public class EmptyCell implements Cell {
 
     @Override
     public boolean isChecked() {
-        return cellState.isChecked();
+        return cellState.isOpened();
     }
 
     @Override
--- src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/LandMineCell.java
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/LandMineCell.java
@@ -27,7 +27,7 @@ public class LandMineCell implements Cell {
 
     @Override
     public boolean isChecked() {
-        return cellState.isChecked();
+        return cellState.isFlagged();
     }
 
     @Override
