6e7dd5cd26db4fa06bb879434b744c5d7247c1fd
Cheolhyun Mun <zooxop@gmail.com>
2024-10-08 16:25:48 +0900

    init: mine 패키지에 tobe 복제

---
+++ src/main/java/cleancode/studycafe/mine/StudyCafeApplication.java
@@ -0,0 +1,10 @@
+package cleancode.studycafe.mine;
+
+public class StudyCafeApplication {
+
+    public static void main(String[] args) {
+        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
+        studyCafePassMachine.run();
+    }
+
+}
---
+++ src/main/java/cleancode/studycafe/mine/exception/AppException.java
@@ -0,0 +1,9 @@
+package cleancode.studycafe.mine.exception;
+
+public class AppException extends RuntimeException {
+
+    public AppException(String message) {
+        super(message);
+    }
+
+}
