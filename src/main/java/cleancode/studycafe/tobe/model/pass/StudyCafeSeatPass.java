5c54d68a923e7e2239b460ddfa94679685a8a09a
Cheolhyun Mun <zooxop@gmail.com>
2024-10-10 15:36:17 +0900

    [6-3] 리팩토링(2) - PassOrder 개념 추출

--- src/main/java/cleancode/studycafe/tobe/model/pass/StudyCafeSeatPass.java
+++ src/main/java/cleancode/studycafe/tobe/model/pass/StudyCafeSeatPass.java
@@ -48,7 +48,7 @@ public class StudyCafeSeatPass implements StudyCafePass {
         return price;
     }
 
-    public double getDiscountRate() {
-        return discountRate;
+    public int getDiscountPrice() {
+        return (int) (price * discountRate);
     }
 }
